845407cd7f7c88f09032ab02c03d9f988cf9a3f1
jessicagaines <59629172+jessicagaines@users.noreply.github.com>
2024-03-20 15:08:10 -0700

    Delete a few things still in development

--- factories.py
+++ factories.py
@@ -4,9 +4,9 @@ Created on Mon Aug 30 15:55:32 2021
 
 @author: Jessica Gaines
 """
-from control_law import ControlLaw,AdaptiveControlLaw,AdaptiveControlLaw_Target
-from observer import Observer,AdaptiveObserverState,AdaptiveObserverFeedback, ObserverFixed, ObserverUnfixed
-from target import Target,AdaptiveTarget,AdaptiveTargetSimple
+from control_law import ControlLaw
+from observer import Observer
+from target import Target
 
 '''
 Factory methods for dynamic creation of different implementations of model components
@@ -15,38 +15,20 @@ Factory methods for dynamic creation of different implementations of model compo
 def ControlLawFactory(controller_config,plant):
     controller = None
     if 'adaptation_type' in controller_config:
-        if controller_config['adaptation_type'] == 'control_output':
-            controller = AdaptiveControlLaw(controller_config,plant)
-        elif controller_config['adaptation_type'] == 'control_target':
-            controller = AdaptiveControlLaw_Target(controller_config,plant)
-        else: print('Unrecognized controller type')
+        print('implementation in progress')
     else: 
         controller = ControlLaw(controller_config,plant)
     return controller
 
 def ObserverFactory(observer_config,plant,ts,aud_delay=None,som_delay=None,estimated_arn=None,estimated_srn=None):
     observer = None
-    if 'adaptation_type' in observer_config:
-        if observer_config['adaptation_type'] == 'prediction':
-            observer = AdaptiveObserverState(observer_config,plant,ts,aud_delay,som_delay)
-        elif observer_config['adaptation_type'] == 'simple_target':
-            observer = Observer(observer_config,plant,ts,aud_delay,som_delay)
-        elif observer_config['adaptation_type'] == 'matlab':
-            observer = AdaptiveObserverFeedback(observer_config,plant,ts,aud_delay,som_delay)
-        else: print('Unrecognized observer type')
     observer = Observer(observer_config,plant,ts,aud_delay,som_delay,estimated_arn,estimated_srn)
     return observer
 
 def TargetFactory(target_config,plant):
     target = None
     if 'adaptation_type' in target_config:
-        if target_config['adaptation_type'] == 'matlab':
-            target = AdaptiveTarget(target_config,plant)
-        elif target_config['adaptation_type'] == 'simple_target':
-            target = AdaptiveTargetSimple(target_config,plant)
-        else: 
-            print('Unrecognized target type. Creating normal target')
-            target = Target(target_config,plant)
+        print('implementation in progress')
     else: 
         target = Target(target_config,plant)
     return target
